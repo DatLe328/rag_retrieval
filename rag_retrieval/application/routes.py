17f161fa2acffc630a6353d3193eac40c8116d91
DatLe328 <lvdat1705@gmail.com>
2025-10-09 09:17:23 +0000

    feat: add validator, debug panel

--- rag_retrieval/application/routes.py
+++ rag_retrieval/application/routes.py
@@ -7,20 +7,26 @@ bp = Blueprint("rag", __name__)
 @bp.route("/query", methods=["POST"])
 def query():
     data = request.get_json(force=True)
+
     user_query = data.get("query")
     if not user_query:
         return jsonify({"error": "Missing 'query'"}), 400
-    
-    print(Settings.WEAVIATE_HOST)
-    print(Settings.WEAVIATE_GRPC)
+
+    multi_n = int(data["multi_n"]) if "multi_n" in data else Settings.MULTI_QUERY_N
+    top_k = int(data["top_k"]) if "top_k" in data else Settings.RERANK_TOPK
+    alpha = float(data["alpha"]) if "alpha" in data else Settings.HYBRID_ALPHA
+
     result = rag_pipeline(
         user_query,
-        multi_n=int(data.get("multi_n", Settings.MULTI_QUERY_N)),
-        top_k=int(data.get("top_k", Settings.RERANK_TOPK)),
-        alpha=float(data.get("alpha", Settings.HYBRID_ALPHA)),
-        weav_host=Settings.WEAVIATE_HOST, weav_port=Settings.WEAVIATE_PORT, weav_grpc=Settings.WEAVIATE_GRPC,
+        multi_n=multi_n,
+        top_k=top_k,
+        alpha=alpha,
+        weav_host=Settings.WEAVIATE_HOST,
+        weav_port=Settings.WEAVIATE_PORT,
+        weav_grpc=Settings.WEAVIATE_GRPC,
         weav_collection=Settings.WEAVIATE_COLLECTION_NAME,
         chat_conf=(Settings.CHAT_PROVIDER, Settings.CHAT_MODEL),
         reranker_conf=(Settings.RERANKER_PROVIDER, Settings.RERANKER_MODEL)
     )
+
     return jsonify(result)
